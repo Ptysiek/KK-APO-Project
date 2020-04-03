b7a9bbe486db9956a42ec7f300b23217969e1098
Kacu <k.ptychu@gmail.com>
2020-04-03 15:51:27 +0100

    Made an layout test version for AccordionContainer and AccorionNode classes

--- KK17413_APO/KK17413_APO/AccordionContainer.cs
+++ KK17413_APO/KK17413_APO/AccordionContainer.cs
@@ -84,7 +84,7 @@ namespace KK17413_APO
         // ____________________________________________________________________________________________________________________________________
         // ------------------------------------------------------------------------------------------------------------------------------------
         private FlowLayoutPanel frame;
-        private List<AccordionNode> Items;
+        private List<AccordionNode> items;
 
 
 
@@ -93,7 +93,31 @@ namespace KK17413_APO
         public AccordionContainer()
         {
             frame = new FlowLayoutPanel();
-            Items = new List<AccordionNode>();
+            items = new List<AccordionNode>();
+
+
+            items.Add(new AccordionNode());
+            items.Add(new AccordionNode());
+            items.Add(new AccordionNode());
+            items.Add(new AccordionNode());
+
+
+            frame.Dock = DockStyle.Fill;
+            frame.AutoScroll = true;
+            //frame.AutoScrollMinSize = new Size(100, 100) ;
+
+
+            frame.WrapContents = false;
+            frame.FlowDirection = FlowDirection.TopDown;
+
+
+            //frame..Add
+            foreach (var item in items)
+            {
+                frame.Controls.Add(item.Control);
+                //frame.Container.Add(item);
+            }
+
         }
     }
 
@@ -103,16 +127,72 @@ namespace KK17413_APO
     // ########################################################################################################################################
     class AccordionNode
     {
+        public Control Control { get { return workspace; } }
+
+
         private SplitContainer workspace;
         private SplitterPanel headPanel;
         private SplitterPanel bodyPanel;
+
+        private Button headPanel_Button;
+        private Label headPanel_Text;
+
+        int Collapsed_height;
 
         public AccordionNode()
         {
             workspace = new SplitContainer();
             headPanel = workspace.Panel1;
             bodyPanel = workspace.Panel2;
+
+            headPanel_Button = new Button();
+            headPanel_Text = new Label();
+
+            Collapsed_height = headPanel_Text.Height - 10;
+
+            workspace.Orientation = Orientation.Horizontal;
+            //workspace.SplitterDistance = Collapsed_height;
+            workspace.IsSplitterFixed = true;
+            workspace.Panel1Collapsed = false;
+            workspace.Panel2Collapsed = true;
+            workspace.Panel1MinSize = 1;
+            workspace.Panel2MinSize = 1;
+            workspace.Height = Collapsed_height;
+
+            headPanel.BackColor = Color.Yellow;            
+            bodyPanel.BackColor = Color.Orange;
+
+
+            headPanel_Button.Text = "";
+            headPanel_Button.BackColor = Color.White;
+            headPanel_Button.Height = headPanel_Text.Height - 10;
+            headPanel_Button.Width = headPanel_Button.Height;
+
+            headPanel_Text.Text = "AccordionNode";
+            headPanel_Text.Left = headPanel_Button.Width;
+
+
+            headPanel.Controls.Add(headPanel_Button);
+            headPanel.Controls.Add(headPanel_Text);
+
+
+            headPanel_Button.Click += new EventHandler(hp_Button_Click);
         }
 
+
+        private void hp_Button_Click(object sender, EventArgs e)
+        {
+            workspace.Panel2Collapsed = !workspace.Panel2Collapsed;
+
+            if(workspace.Panel2Collapsed)
+            {
+                workspace.Height = Collapsed_height;
+            }
+            else
+            {
+                workspace.Height = 100;
+                workspace.SplitterDistance = Collapsed_height;
+            }
+        }
     }
 }

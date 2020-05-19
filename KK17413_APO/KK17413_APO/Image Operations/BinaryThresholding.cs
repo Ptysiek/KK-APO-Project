1cca2df5515307d4247a0ebe82c2b0f6c0a02015
Kacu <k.ptychu@gmail.com>
2020-05-19 12:04:28 +0000

    New File: BinaryThresholding

---
+++ KK17413_APO/KK17413_APO/Image Operations/BinaryThresholding.cs
@@ -0,0 +1,38 @@
+﻿using KK17413_APO.Data_Structures;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Emgu.CV;
+using Emgu.Util;
+using Emgu.CV.Structure;
+
+
+
+namespace KK17413_APO.Image_Operations
+{
+    class BinaryThresholding
+    {
+        public static ImageData GetResult(ImageData oldOne, ref ProgressBar pbar)
+        {
+            if (oldOne.Ready)
+                return oldOne;
+
+
+            Mat img = imread("./LENA_512.jpg", 1);
+            cvtColor(img, img, CV_BGR2GRAY);
+
+            for (int i = 1; i < 10; i++)
+            {
+                Mat binary_img;
+                double border = 100 + 10 * i;
+                threshold(img, binary_img, border, 255, THRESH_BINARY);
+            }
+
+            return oldOne;
+        }
+    }
+}

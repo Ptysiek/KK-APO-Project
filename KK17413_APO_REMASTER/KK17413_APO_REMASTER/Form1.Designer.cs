8875e5a7e5c57882c2a3f4ab2d789d9a127673a5
Kacu <k.ptychu@gmail.com>
2020-05-20 08:45:23 +0000

    New Functionality: Binary Thresholding

--- KK17413_APO_REMASTER/KK17413_APO_REMASTER/Form1.Designer.cs
+++ KK17413_APO_REMASTER/KK17413_APO_REMASTER/Form1.Designer.cs
@@ -28,48 +28,98 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.imageBox1 = new Emgu.CV.UI.ImageBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.operationsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tresholdingToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.binaryzationToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(48, 81);
+            this.pictureBox1.Location = new System.Drawing.Point(12, 63);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(409, 337);
+            this.pictureBox1.Size = new System.Drawing.Size(375, 375);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
             // 
-            // imageBox1
+            // menuStrip1
             // 
-            this.imageBox1.Location = new System.Drawing.Point(509, 112);
-            this.imageBox1.Name = "imageBox1";
-            this.imageBox1.Size = new System.Drawing.Size(244, 297);
-            this.imageBox1.TabIndex = 2;
-            this.imageBox1.TabStop = false;
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.operationsToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 28);
+            this.menuStrip1.TabIndex = 3;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // operationsToolStripMenuItem
+            // 
+            this.operationsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tresholdingToolStripMenuItem});
+            this.operationsToolStripMenuItem.Name = "operationsToolStripMenuItem";
+            this.operationsToolStripMenuItem.Size = new System.Drawing.Size(96, 24);
+            this.operationsToolStripMenuItem.Text = "Operations";
+            // 
+            // tresholdingToolStripMenuItem
+            // 
+            this.tresholdingToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.binaryzationToolStripMenuItem});
+            this.tresholdingToolStripMenuItem.Name = "tresholdingToolStripMenuItem";
+            this.tresholdingToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.tresholdingToolStripMenuItem.Text = "Tresholding";
+            // 
+            // binaryzationToolStripMenuItem
+            // 
+            this.binaryzationToolStripMenuItem.Name = "binaryzationToolStripMenuItem";
+            this.binaryzationToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.binaryzationToolStripMenuItem.Text = "Binaryzation";
+            this.binaryzationToolStripMenuItem.Click += new System.EventHandler(this.binaryzationToolStripMenuItem_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(393, 63);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(395, 375);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 4;
+            this.pictureBox2.TabStop = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.imageBox1);
+            this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.PictureBox pictureBox1;
-        private Emgu.CV.UI.ImageBox imageBox1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem operationsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem tresholdingToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem binaryzationToolStripMenuItem;
+        private System.Windows.Forms.PictureBox pictureBox2;
     }
 }
 
--- KK17413_APO_REMASTER/KK17413_APO_REMASTER/Form1.cs
+++ KK17413_APO_REMASTER/KK17413_APO_REMASTER/Form1.cs
@@ -7,6 +7,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.CV.Structure;
+
 
 namespace KK17413_APO_REMASTER
 {
@@ -15,6 +18,27 @@ namespace KK17413_APO_REMASTER
         public Form1()
         {
             InitializeComponent();
+
+            
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void binaryzationToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Image<Bgra, byte> image = new Image<Bgra, byte>("C:\\Users\\kptyc\\Desktop\\lena_color.png");
+            Image<Gray, byte> gray = image.Convert<Gray, byte>();
+
+            // Making image with black background:
+            Image<Gray, byte>  binarize = new Image<Gray, byte>(gray.Width, gray.Height, new Gray(0));
+
+            CvInvoke.Threshold(gray, binarize, 50, 150, Emgu.CV.CvEnum.ThresholdType.Binary);
+
+            pictureBox1.Image = image.Bitmap;
+            pictureBox2.Image = binarize.Bitmap;
         }
     }
 }

6d3eb0e4ab931f9980c83bfe0e0302e38495daee
Kacu <k.ptychu@gmail.com>
2020-05-24 09:54:04 +0000

    new Language_Factory

--- KK17413_APO_REMASTER/KK17413_APO_REMASTER/BackEnd/Factories/Language_Factory.cs
+++ KK17413_APO_REMASTER/KK17413_APO_REMASTER/BackEnd/Factories/Language_Factory.cs
@@ -1,12 +1,59 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace KK17413_APO_REMASTER.BackEnd.Factories
 {
-    class Language_Factory
+    public class Language_Factory
     {
+        //private Language currentLanguage = null;   // Currently chosen language
+        private Language currentLanguage;   // Currently chosen language
+
+
+        private Dictionary<string, Language> languageList = new Dictionary<string, Language>()
+        {
+            { "PL", new PL_Language() },
+            { "ANG", new ANG_Language() }
+        };
+
+
+        // ##########################################################################################################
+        public Language_Factory()
+        {
+            // Set default language:
+            SetLanguage("ANG");
+        }
+        public Language_Factory(string key)
+        {
+            // Set start language:
+            SetLanguage(key);
+        }
+
+
+        // ##########################################################################################################
+        public string GetValue(string key)
+        {
+            if (currentLanguage == null) return null;
+            return currentLanguage.GetValue(key);
+        }
+
+        public bool SetLanguage(string key)
+        {
+            if (!languageList.ContainsKey(key)) return false;
+
+            currentLanguage = languageList[key];
+            return true;
+        }
+        public List<string> Keys()
+        {
+            List<string> result = new List<string>();
+            foreach (string key in languageList.Keys)
+            {
+                result.Add(key);
+            }
+            result.Sort();
+            return result;
+        }
+        // ##########################################################################################################
     }
 }

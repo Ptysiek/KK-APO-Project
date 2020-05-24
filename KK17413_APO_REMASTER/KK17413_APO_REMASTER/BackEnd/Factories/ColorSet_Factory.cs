e92ef4fad8ca2168e06fc523b0c4b631df0d3a58
Kacu <k.ptychu@gmail.com>
2020-05-24 10:01:50 +0000

    New ColorSet_Factory

---
+++ KK17413_APO_REMASTER/KK17413_APO_REMASTER/BackEnd/Factories/ColorSet_Factory.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+
+
+namespace KK17413_APO_REMASTER.BackEnd.Factories
+{
+    public class ColorSet_Factory
+    {
+        public readonly Color Transparent = Color.Maroon;
+
+        //private Language currentLanguage = null;   // Currently chosen language
+        private ColorManager currentColorSet;   // Currently chosen set
+        private const string defaultSet = "VSDarkTheme";
+
+        private Dictionary<string, ColorManager> colorSetList = new Dictionary<string, ColorManager>()
+        {
+            { "VSDarkTheme", new Vs_ColorSet() },
+            { "WFBrightTheme", new WFBright_ColorSet() },
+            { "TrevilamTheme", new Trevilam_ColorSet() },
+            { "FishBonesTheme", new FishBonesTheme_ColorSet() },
+            { "VulturTheme", new VulturTheme_ColorSet() }
+        };
+
+
+        // ##########################################################################################################
+        public ColorSet_Factory()
+        {
+            // Set default color set:
+            SetColorSet(defaultSet);
+        }
+        public ColorSet_Factory(string key)
+        {
+            // Set start color set:
+            SetColorSet(key);
+        }
+
+
+        // ##########################################################################################################
+        public Color GetValue(string key)
+        {
+            if (currentColorSet == null) return Color.Empty;
+            return currentColorSet.GetValue(key);
+        }
+
+        public bool SetColorSet(string key)
+        {
+            if (!colorSetList.ContainsKey(key))
+            {
+                currentColorSet = colorSetList[defaultSet];
+                return false;
+            }
+            currentColorSet = colorSetList[key];
+            return true;
+        }
+        public List<string> Keys()
+        {
+            List<string> result = new List<string>();
+            foreach (string key in colorSetList.Keys)
+            {
+                result.Add(key);
+            }
+            result.Sort();
+            return result;
+        }
+        // ##########################################################################################################
+    }
+}
